e282b79863b439f2f50c8761d39b1106d9ee97d9
Kirill Kovalchuk <kirillkovalchukstudy@gmail.com>
2025-08-23 16:42:55 +0300

    Add files via upload

---
+++ PR-SUMMER-MAP/collisionsblockclass.js
@@ -0,0 +1,14 @@
+class collisionsBlock{
+   constructor({position}){
+      this.position = position;
+      this.width=150;
+      this.height=25;
+   }
+   draw(){
+      c.fillStyle = 'rgba(255,0,0,0.5)';
+      c.fillRect(this.position.x,this.position.y,this.width,this.height)
+   }
+   update(){
+      this.draw()
+   }
+}
